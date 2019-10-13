2ba506da2d2838e03b4a8b79e3fa193897e7da15
Tamasi Kar <tamasi.kar@gmail.com>
2019-10-13 14:15:27 +0200

    compilation script

--- compileGen_run_1.sh
+++ compileGen_run_1.sh
@@ -1,7 +1,7 @@
 #g++ short-example.cc -o short-example \
 #`fastjet-install/bin/fastjet-config --cxxflags --libs --plugins`
 #./short-example
-g++ TrigEff.cc Rate_sumpt.cc Gen_Jet.cc -o genjet \
+g++ Gen_output.cc Gen_Jet.cc -o genjet \
 `root-config --cflags --libs` \
 `fastjet-install/bin/fastjet-config --cxxflags --libs --plugins`
 #./trackjet
