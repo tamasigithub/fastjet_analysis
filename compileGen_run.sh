eff242354b225da129fa2fa27e3e19424e7cef92
Tamasi Kar <tamasi.kar@gmail.com>
2019-11-06 17:36:44 +0100

    making all the files consistent with name

--- compileGen_run.sh
+++ compileGen_run.sh
@@ -1,7 +1,7 @@
 #g++ short-example.cc -o short-example \
 #`fastjet-install/bin/fastjet-config --cxxflags --libs --plugins`
 #./short-example
-g++ CaloEmu.cc Gen_output.cc Gen_Jet_1.cc -o genjet_1 \
+g++ CaloEmu.cc Gen_output.cc Gen_Jet.cc -o genjet \
 `root-config --cflags --libs` \
 `fastjet-install/bin/fastjet-config --cxxflags --libs --plugins`
 #./trackjet
